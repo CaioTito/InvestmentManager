fa36753fb617affd9d7353f5031acac612c73849
Caio Souza da Silva <caio_tito@hotmail.com>
2024-02-27 14:41:23 -0300

    Test: Adicionado teste para command de criação de categoria.

---
+++ InvestmentManager.UnitTests/Application/Command/CreateCategoryCommandHandlerTest.cs
@@ -0,0 +1,36 @@
+﻿using InvestmentManager.Application.Commands;
+using InvestmentManager.Domain.Entities;
+using InvestmentManager.Domain.Interfaces.Repositories;
+using InvestmentManager.UnitTests.Mocks;
+using Moq;
+using Moq.AutoMock;
+
+namespace InvestmentManager.UnitTests.Application.Command
+{
+    public class CreateCategoryCommandHandlerTest
+    {
+        private readonly AutoMocker _mocker;
+        private readonly CreateCategoryCommanHandler _createCategoryCommanHandler;
+
+        public CreateCategoryCommandHandlerTest()
+        {
+            _mocker = new AutoMocker();
+            _createCategoryCommanHandler = new CreateCategoryCommanHandler(_mocker.GetMock<ICategoriesRepository>().Object);
+        }
+
+        [Fact]
+        public async Task InputDataIsOK_Executed_ReturnProjectId()
+        {
+            //Arrange
+            var createCategoryCommand = CategoryMock.CreateProjectCommandFaker.Generate();
+
+            //Act
+            var id = await _createCategoryCommanHandler.Handle(createCategoryCommand, new CancellationToken());
+
+            //Assert
+            Assert.IsType<Guid>(id);
+
+            _mocker.GetMock<ICategoriesRepository>().Verify(c => c.AddAsync(It.IsAny<Category>()), Times.Once);
+        }
+    }
+}
---
+++ InvestmentManager.UnitTests/Mocks/CategoryMock.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using InvestmentManager.Application.Commands;
+using InvestmentManager.Domain.Entities;
+
+namespace InvestmentManager.UnitTests.Mocks
+{
+    public static class CategoryMock
+    {
+        public static Faker<Category> CategoryFaker => new Faker<Category>()
+            .CustomInstantiator(f => (
+                new Category(f.Random.Word())
+            ));
+
+        public static Faker<CreateCategoryCommand> CreateProjectCommandFaker =>
+                new Faker<CreateCategoryCommand>()
+                    .RuleFor(x => x.Name, f => f.Random.Word());
+    }
+}
